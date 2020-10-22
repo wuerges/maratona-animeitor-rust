167b7cb8875b2bf6be0beffca23e785e93145664
Emilio Wuerges <wuerges@gmail.com>
2020-10-22 19:20:33 -0300

    testing styles

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use warp::Filter;
-use maratona_animeitor_rust::*;
-
-use data::*;
+use maratona_animeitor_rust::data::*;
 
 #[tokio::main]
 async fn main() {
 
-    let runs = RunsPanel::from_file("test/sample/runs").unwrap();
+    let runs = Arc::new(
+        Mutex::new(
+            RunsPanel::from_file("test/sample/runs").unwrap()
+        ));
+    let runs = warp::any().map(move || Arc::clone(&runs));
+
+    let routes = warp::path("runs").and(runs.clone()).and_then(serve_runs);
 
 
-
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| {
-            let j = serde_json::to_string(&runs.latest_n(10)).unwrap();
-            format!("Hello, {}!", name)
-        });
-
-    warp::serve(hello)
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+async fn serve_runs(runs : Arc<Mutex<RunsPanel>>) 
+-> Result<impl warp::Reply, warp::Rejection> {
+    let db = runs.lock().await;
+    let r = serde_json::to_string(&*db.latest_n(10)).unwrap();
+    Ok(r)
 }
--- static/runPanel.js
+++ static/runPanel.js
@@ -2,69 +2,76 @@
 
 
 function App() {
-    const [devs, setDevs] = React.useState([
-      {
-        name: "Sabino",
-        score: 5,
-      },
-      {
-        name: "Emilio",
-        score: 3,
-      },
-      {
-        name: "Charie Brown",
-        score: 2,
-      },
-    ]);
+
+    const [devs, setDevs] = React.useState(
+        [
+            {"id":197701401,"time":2,"team_login":"teambrbr4","prob":"H","answer":"Yes"},
+            {"id":198342704,"time":3,"team_login":"teambrbr41","prob":"H","answer":"Yes"},
+            {"id":198320203,"time":3,"team_login":"teambrbr14","prob":"H","answer":"Yes"},
+            {"id":198273802,"time":3,"team_login":"teambrbr42","prob":"H","answer":"Yes"},
+            {"id":198956310,"time":4,"team_login":"teambrbr26","prob":"H","answer":"Yes"},
+            {"id":198740009,"time":4,"team_login":"teambrbr32","prob":"H","answer":"Yes"},
+            {"id":198728208,"time":4,"team_login":"teambrbr64","prob":"H","answer":"Yes"},
+            {"id":198503107,"time":4,"team_login":"teambrbr54","prob":"H","answer":"Yes"},
+            {"id":198475606,"time":4,"team_login":"teambrbr56","prob":"H","answer":"Yes"},
+            {"id":198447905,"time":4,"team_login":"teambrbr51","prob":"H","answer":"Yes"}
+        ]);
+    // const [devs, setDevs] = React.useState([
+    //   {
+    //     name: "Sabino",
+    //     score: 5,
+    //   },
+    //   {
+    //     name: "Emilio",
+    //     score: 3,
+    //   },
+    //   {
+    //     name: "Charie Brown",
+    //     score: 2,
+    //   },
+    // ]);
+    // const [devs, setDevs] = React.useState([]);
+    // React.useEffect(() => {
+    //     fetch("http://localhost:3030/runs")
+    //         .then(res => res.json())
+    //         .then(
+    //             (result) => { setDevs(result); },
+    //             (error) => {}
+    //         )
+    // }, []);
   
     const addInDev = (dev) => {
       const i = devs.indexOf(dev);
       const copyDevs = [...devs];
       copyDevs[i].score++;
-      copyDevs[i].isChanging = true;
+    //   copyDevs[i].isChanging = true;
       setDevs(copyDevs.sort((a, b) => b.score - a.score));
     };
   
     return (
       <>
-        <table border="1">
+        <div border="1" style={{width: "400px"}}>
           {devs.map((dev) => (
-            <tr
-              key={dev.name}
+              <div
+              key={dev.id}
               style={{
-                zIndex: dev.isChanging ? 10 : 5,
-                position: "absolute",
-                top: devs.indexOf(dev) * 40,
-                transition: "1s ease top",
-              }}
-            >
-              <td>{dev.name}</td>
-              <td>{dev.score}</td>
-            </tr>
+                  'border-style': "solid",
+                  'border-width' : "1px",
+                  padding: '5px',
+                  zIndex: dev.id,
+                  position: "absolute",
+                  top: devs.indexOf(dev) * 40,
+                  transition: "1s ease top",
+                }}
+                >
+                <div style={{float: "left", width: '200px',  margin: '5px', 'border-style': "solid"}}>{dev.team_login}</div>
+                <div style={{float: "left", margin: '5px', 'border-style': "solid"}}>{dev.prob}</div>
+                <div style={{float: "left", margin: '5px', 'border-style': "solid"}}>{dev.answer}</div>
+            </div>
           ))}
-        </table>
-  
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-  
-        {devs.map((dev) => (
-          <div>
-            <button onClick={() => addInDev(dev)}>{dev.name}</button>
-          </div>
-        ))}
+        </div>
       </>
     );
-    
-    return <p>Hello World!</p>
   }
   
   ReactDOM.render(
