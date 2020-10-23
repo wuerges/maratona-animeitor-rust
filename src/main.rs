c58d438ae2b97537579b3eaf116a8a2ec918b829
Emilio Wuerges <wuerges@gmail.com>
2020-10-23 14:06:37 -0300

    working with shared state variable

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use std::sync::Arc;
-use tokio::sync::Mutex;
+use tokio::{sync::Mutex, spawn};
+use tokio;
+use std::thread;
 use warp::Filter;
 use maratona_animeitor_rust::data::*;
 
 #[tokio::main]
 async fn main() {
 
-    let runs = Arc::new(
-        Mutex::new(
-            RunsPanel::from_file("test/sample/runs").unwrap()
-        ));
-    let runs = warp::any().map(move || Arc::clone(&runs));
+    let arc_runs = Arc::new(Mutex::new(RunsPanel::empty()));
 
+    let shared = Arc::clone(&arc_runs);
+    spawn(async move {
+        let dur = tokio::time::Duration::new(1, 0);
+        let mut interval = tokio::time::interval(dur);
+        loop {
+            interval.tick().await;
+            update_runs(shared.clone()).await;
+        }        
+    });
+
+    let runs = warp::any().map(move || arc_runs.clone());
 
     let static_assets = warp::path("static").and(warp::fs::dir("static"));
     let runs = warp::path("runs").and(runs.clone()).and_then(serve_runs);
@@ -19,9 +28,15 @@ async fn main() {
     let routes = static_assets.or(runs);
 
 
+
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+async fn update_runs(runs : Arc<Mutex<RunsPanel>>) {
+    let mut db = runs.lock().await;
+    *db = RunsPanel::from_file("test/sample/runs").unwrap();  
 }
 
 async fn serve_runs(runs : Arc<Mutex<RunsPanel>>) 
