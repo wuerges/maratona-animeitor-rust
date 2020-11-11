5dbaa3f0f5ef8c009628bce7cc5ad2dfdd94e498
Emilio Wuerges <wuerges@gmail.com>
2020-11-11 18:36:09 -0300

    made navigation force refresh

--- lib-seed/src/navigation.rs
+++ lib-seed/src/navigation.rs
@@ -9,7 +9,8 @@ use seed::{prelude::*, *};
 
 extern crate rand;
 
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
+    orders.subscribe(Msg::UrlChanged);
     Model {
     }
 }
@@ -18,22 +19,22 @@ struct Model {
 }
 
 enum Msg {
-    // UrlChanged(subs::UrlChanged)
+    UrlChanged(subs::UrlChanged)
 }
 
-fn update(_: Msg, _: &mut Model, _: &mut impl Orders<Msg>) {
-    // match msg {
-        // Msg::UrlChanged(subs::UrlChanged(url)) => {
-            // url.go_and_load();
-        // }
-    // }
+fn update(msg: Msg, _: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::UrlChanged(subs::UrlChanged(url)) => {
+            url.go_and_load();
+        }
+    }
 }
 
 fn build_url_filter(sede : &configdata::Sede) -> String {
-    // Url::new()
-    // .add_path_part("seed")
-    // .add_path_part("everything.html")
-    Url::current()
+    Url::new()
+    .add_path_part("seed")
+    .add_path_part("everything2.html")
+    // Url::current()
     .set_search(UrlSearch::new(vec![
         // ("source", vec![&sede.parent_source]),
         ("sede", vec![&sede.name]),
