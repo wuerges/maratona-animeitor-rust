193c2b71e72350c76f600d44a696153854a56f5d
Emilio Wuerges <wuerges@gmail.com>
2020-11-13 14:00:58 -0300

    checkd contest number and site number on auth

--- turbineitor/src/auth.rs
+++ turbineitor/src/auth.rs
@@ -1,8 +1,9 @@
 use maratona_animeitor_rust::auth::UserKey;
+use crate::Params;
+use crate::errors::Error;
 
 //use serde::{Serialize, Deserialize};
 use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey};
-use crate::errors::Error;
 
 pub fn sign_user_key(user_key : UserKey, secret : &str) -> Result<String, Error> {
     Ok(encode(
@@ -11,9 +12,19 @@ pub fn sign_user_key(user_key : UserKey, secret : &str) -> Result<String, Error>
         &EncodingKey::from_secret(secret.as_ref()))?)
 }
 
-pub fn verify_user_key(token : &String, secret : &str) -> Result<UserKey, Error> {
+pub fn verify_user_key(token : &String, secret : &str, params: &Params) -> Result<UserKey, Error> {
     let user_key = decode::<UserKey>(token, 
-        &DecodingKey::from_secret(secret.as_ref()), 
+        &DecodingKey::from_secret(secret.as_ref()),        
         &Validation::default())?;
-    Ok(user_key.claims)
+
+    let claims = user_key.claims;
+
+    if params.contest_number != claims.contest_number {
+        return Err(Error::WrongContestNumber(params.contest_number, claims.contest_number));
+    }
+    if params.site_number != claims.site_number {
+        return Err(Error::WrongSiteNumber(params.site_number, claims.site_number));
+    }
+    
+    Ok(claims)
 }
--- turbineitor/src/errors.rs
+++ turbineitor/src/errors.rs
@@ -4,4 +4,10 @@ use thiserror::Error;
 pub enum Error {
     #[error("error with the jwt token")]
     JWTError(#[from] jsonwebtoken::errors::Error),
+
+    #[error("Wrog contest number: (exected: {0}, found: {1})")]
+    WrongContestNumber(i32, i32),
+
+    #[error("Wrog site number: (exected: {0}, found: {1})")]
+    WrongSiteNumber(i32, i32),
 }
