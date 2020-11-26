e6959382086c738ef376a13f38f01acd3d11ab51
Emilio Wuerges <wuerges@gmail.com>
2020-11-26 15:48:13 -0300

    automatic scoreboard wonking with websockets

--- client/src/automatic.rs
+++ client/src/automatic.rs
@@ -1,94 +1,95 @@
 use data;
 use seed::{prelude::*, *};
 
-use crate::requests::*;
 use crate::helpers::*;
+use crate::requests::*;
 use crate::views;
 
 extern crate rand;
 
 fn init(url: Url, orders: &mut impl Orders<Msg>) -> Model {
     orders.subscribe(Msg::UrlChanged);
-    orders.skip().send_msg(Msg::Reload);
-    orders.skip().stream(streams::interval(30_000, || Msg::Reload));
+    orders.perform_cmd(fetch_all());
 
     Model {
-        center : None,
-        url_filter : get_url_filter(&url),
+        center: None,
+        url_filter: get_url_filter(&url),
+        original: data::ContestFile::dummy(),
         contest: data::ContestFile::dummy(),
         runs: data::RunsFile::empty(),
+        ws: None,
+        dirty: true,
     }
 }
 
 struct Model {
-    center : Option<String>,
+    center: Option<String>,
     url_filter: Option<Vec<String>>,
-    contest : data::ContestFile,
+    original: data::ContestFile,
+    contest: data::ContestFile,
     runs: data::RunsFile,
+    ws: Option<WebSocket>,
+    dirty: bool,
 }
 
 enum Msg {
     UrlChanged(subs::UrlChanged),
+    RunUpdate(WebSocketMessage),
     Reload,
-    Recenter,
-    Fetched(
-        fetch::Result<data::RunsFile>,
-        fetch::Result<data::ContestFile>),
+    Fetched(fetch::Result<data::ContestFile>),
 }
 
 async fn fetch_all() -> Msg {
-    let r = fetch_allruns().await;
     let c = fetch_contest().await;
-    Msg::Fetched(r, c)
+    Msg::Fetched(c)
 }
 
 fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
         Msg::UrlChanged(subs::UrlChanged(url)) => {
             model.url_filter = get_url_filter(&url);
-        },
-        Msg::Recenter => {
-            model.center = None;
-        },
+        }
+        // Msg::Recenter => {
+        //     model.center = None;
+        // }
         Msg::Reload => {
-            orders.skip().perform_cmd(fetch_all());
-        },
-        Msg::Fetched(Ok(runs), Ok(contest)) => {
-            
-            model.runs = runs;
-            model.contest = contest;
-
-            log!("received n runs: {}", model.runs.len());
-            
-            for r in &model.runs.sorted() {
-                if r.time < model.contest.score_freeze_time {
-                    let r = model.contest.apply_run(r);
-                    match r {
-                        Ok(()) => (),
-                        Err(e) => log!("run error:", e),
-                    }
+            if model.dirty {
+                model.contest = model.original.clone();
+                for r in &model.runs.sorted() {
+                    model
+                        .contest
+                        .apply_run(r)
+                        .expect("Should be able to apply the run");
                 }
+                model
+                    .contest
+                    .recalculate_placement()
+                    .expect("Should recalculate scores");
+                model.dirty = false;
             }
-            
-            let old_contest = model.contest.clone();
-            model.contest.recalculate_placement().unwrap();
-            for (t1, t2) in model.contest.teams.values().zip(old_contest.teams.values()) {
-                if t1.placement < t2.placement {
-                    model.center = Some(t1.login.clone());
-                    break;
-                }
-            }            
-            
-            orders.perform_cmd(cmds::timeout(10_000, move|| Msg::Recenter));
-            // log!("fetched runs and contest!", model.contest);
-        },
-        Msg::Fetched(Err(e), Ok(_)) => {
-            log!("failed fetching runs: ", e);
-        },
-        Msg::Fetched(_, Err(e)) => {
+        }
+        Msg::RunUpdate(m) => {
+            let run: data::RunTuple = m.json().expect("Should be a RunTuple");
+            if model.runs.refresh_1(&run) {
+                model.dirty = true;
+            }
+        }
+        Msg::Fetched(Ok(contest)) => {
+            model.original = contest;
+            model.dirty = true;
+            model.ws = Some(
+                WebSocket::builder(get_ws_url("/allruns_ws"), orders)
+                    .on_message(Msg::RunUpdate)
+                    .build_and_open()
+                    .expect("Open WebSocket"),
+            );
+            orders
+                .skip()
+                .stream(streams::interval(1_000, || Msg::Reload));
+        }
+        Msg::Fetched(Err(e)) => {
             log!("failed fetching contest: ", e);
-        },
-
+        }
     }
 }
 
@@ -96,6 +97,6 @@ fn view(model: &Model) -> Node<Msg> {
     views::view_scoreboard(&model.contest, &model.center, &model.url_filter)
 }
 
-pub fn start(e : impl GetElement) {
+pub fn start(e: impl GetElement) {
     App::start(e, init, update, view);
 }
