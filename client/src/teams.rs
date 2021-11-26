b3d8ced4ec70afb21755fd547594454fa107452c
Paulo Miranda <paulomirandamss12@gmail.com>
2021-11-25 22:37:49 -0300

    undo changes in teams

--- client/src/teams.rs
+++ client/src/teams.rs
@@ -1,4 +1,3 @@
-use data::configdata;
 use seed::{prelude::*, *};
 
 use crate::requests;
@@ -12,16 +11,16 @@ fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
 }
 
 async fn fetch_all() -> Msg {
-    let c = requests::fetch_config().await;
+    let c = requests::fetch_contest().await;
     Msg::Fetched(c)
 }
 
 struct Model {
-    contest: Option<configdata::ConfigContest>,
+    contest: Option<data::ContestFile>,
 }
 
 enum Msg {
-    Fetched(fetch::Result<configdata::ConfigContest>),
+    Fetched(fetch::Result<data::ContestFile>),
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
@@ -43,27 +42,19 @@ fn view(model: &Model) -> Node<Msg> {
             span!["Failed to load contest config"],
             ],
         Some(contest) => div![
-            contest.teams.iter().map(|team_entry| {
-                let foto_id = format!("foto_{}", &team_entry.login);
+            contest.teams.iter().map(|(team_login, team_entry)| {
+                let foto_id = format!("foto_{}", team_login);
                 div![C!["foto"], id![foto_id],
-                attrs!{At::OnClick =>
-                    std::format!("document.getElementById('foto_{}').style.display = 'none';",
-                    &team_entry.login)
-                },
-                div![C!["nomeTime"], "&team_entry.nome"],
-                &team_entry.foto.as_ref().map(|f|
-                    img![C!["foto_img"],
-                    attrs!{At::Src => std::format!("/static/assets/teams/{}", f)},
-                    attrs!{At::OnError => FAKE}
-                    ]
-                ),
-                &team_entry.musica.as_ref().map(|m|
-                    p![a![C!["musica"], attrs!{At::Href => m}, "Música do Time"]]
-                ),
-                &team_entry.comentario.as_ref().map(|c|
-                    p![C!["comentario"], c]
-                ),
-    ]
+                    attrs!{At::OnClick =>
+                        std::format!("document.getElementById('foto_{}').style.display = 'none';",
+                        &team_entry.login)
+                    },
+                    div![C!["nomeTime"], &team_entry.name],
+                        img![C!["foto_img"],
+                        attrs!{At::Src => std::format!("/static/assets/teams/{}.webp", team_login)},
+                        attrs!{At::OnError => FAKE}
+                    ],
+                ]
             }),
         ],
     }
