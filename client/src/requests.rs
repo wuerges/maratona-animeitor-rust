08fec032c2d2cbeec51351c880765b85e9cb7ec5
Emilio Wuerges <emilio.wuerges@arcadia.com>
2022-04-10 19:24:43 -0300

    getting URL from URL_PREFIX

--- client/src/requests.rs
+++ client/src/requests.rs
@@ -1,8 +1,13 @@
 use seed::prelude::*;
+use std::env::*;
 use data;
 
+pub fn url_prefix() -> &'static str {
+  option_env!("URL_PREFIX").unwrap_or("")
+}
+
 pub async fn fetch_allruns_secret(secret : &String) -> fetch::Result<data::RunsFile> {
-    Request::new(format!("/allruns_{}", secret))
+    Request::new(format!("{}/allruns_{}", url_prefix(), secret))
         .fetch()
         .await?
         .check_status()?
@@ -12,7 +17,7 @@ pub async fn fetch_allruns_secret(secret : &String) -> fetch::Result<data::RunsF
 
 
 pub async fn fetch_contest() -> fetch::Result<data::ContestFile> {
-    Request::new("/contest")
+    Request::new(format!("{}/contest", url_prefix()))
         .fetch()
         .await?
         .check_status()?
@@ -22,7 +27,7 @@ pub async fn fetch_contest() -> fetch::Result<data::ContestFile> {
 
 
 pub async fn fetch_config() -> fetch::Result<data::configdata::ConfigContest> {
-    Request::new("/config")
+    Request::new(format!("{}/config", url_prefix()))
         .fetch()
         .await?
         .check_status()?
