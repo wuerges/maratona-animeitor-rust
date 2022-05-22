fb78b1df6bb993a8fadaf3f62f2e25672ebfafab
Emilio Wuerges <emilio.wuerges@arcadia.com>
2022-05-22 19:29:01 -0300

    timer test

--- server/src/websockets.rs
+++ server/src/websockets.rs
@@ -56,3 +56,53 @@ pub async fn serve_all_runs_ws(
 
     tokio::task::spawn(fut);
 }
+
+
+#[cfg(test)]
+mod tests {
+    use tokio::sync::broadcast;
+    use warp::Filter;
+    use data::TimerData;
+    use crate::websockets::serve_timer_ws;
+    use warp::filters::ws::Message;
+    use serde_json;
+
+    #[tokio::test]
+    async fn test_serve_timer_ws() {
+        let (time_tx, _) : (broadcast::Sender::<TimerData>, _) = broadcast::channel(1000000);
+        let send_time_tx = time_tx.clone();
+
+        let timer = warp::path("timer")
+            .and(warp::ws())
+            .and(warp::any().map(move || time_tx.subscribe()))
+            .map(|ws: warp::ws::Ws, tx| ws.on_upgrade(move |ws| serve_timer_ws(ws, tx)));
+
+
+        let expected1 = Message::text(serde_json::to_string(&TimerData::new(1, 2)).unwrap());
+        let expected2 = Message::text(serde_json::to_string(&TimerData::new(2, 2)).unwrap());
+
+        let mut client = warp::test::ws()
+            .path("/timer")
+            .handshake(timer.clone())
+            .await
+            .expect("handshake");
+
+        {
+            let mut client2 = warp::test::ws()
+                .path("/timer")
+                .handshake(timer)
+                .await
+                .expect("handshake");
+
+            send_time_tx.send(TimerData::new(1, 2)).expect("to send message");
+
+            assert_eq!(client2.recv().await.expect("to receive message"), expected1);
+        }
+
+        assert_eq!(client.recv().await.expect("to receive message"), expected1);
+
+        send_time_tx.send(TimerData::new(2, 2)).expect("to send message");
+
+        assert_eq!(client.recv().await.expect("to receive message"), expected2);
+    }
+}
