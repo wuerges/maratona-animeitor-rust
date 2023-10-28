f882d326bef22a0c1f7781af4dbb88ba66df1705
Emilio Wuerges <wuerges@gmail.com>
2023-10-28 17:21:34 -0300

    rendering timer with option

--- client-v2/src/timer.rs
+++ client-v2/src/timer.rs
@@ -1,32 +1,18 @@
 use leptos::*;
-use serde::Deserialize;
+
+use data::TimerData;
 
 use crate::ws_component::create_websocket_signal;
 
-#[derive(Deserialize, Clone, PartialEq, Eq)]
-struct Timer {
-    pub current_time: i32,
-    pub score_freeze_time: i32,
-}
+fn create_timer() -> ReadSignal<Option<TimerData>> {
+    let timer_message = create_websocket_signal("ws://localhost:9000/api/timer", None);
 
-impl Default for Timer {
-    fn default() -> Self {
-        let max = 60 * 60 * 60;
-        Self {
-            current_time: max - 1,
-            score_freeze_time: max,
-        }
-    }
-}
-
-fn create_timer() -> ReadSignal<Timer> {
-    let timer_message = create_websocket_signal("ws://localhost:9000/api/timer", Timer::default());
-
-    let (timer, set_timer) = create_signal(Timer::default());
+    let (timer, set_timer) = create_signal(None);
 
     create_effect(move |_| {
         let next = timer_message.get();
-        if next != timer.get() {
+
+        if next.is_some() && next != timer.get() {
             set_timer.set(next);
         }
     });
@@ -38,10 +24,13 @@ fn create_timer() -> ReadSignal<Timer> {
 pub fn Timer() -> impl IntoView {
     let timer = create_timer();
 
-    let current_time = move || timer.get().current_time;
-    let score_freeze_time = move || timer.get().score_freeze_time * 60;
-
-    view! {
-        <p> Time?: "`"{current_time}/{score_freeze_time}"'" </p>
+    move || match timer.get() {
+        Some(TimerData {
+            current_time,
+            score_freeze_time,
+        }) => view! {
+            <p> Time?: "`"{current_time}/{score_freeze_time * 60}"'" </p>
+        },
+        None => view! {<p> Timer is none =/ </p>},
     }
 }
