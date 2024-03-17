40506f81c23e162454b6ac4e75afa8d47c299066
Emilio Wuerges <wuerges@gmail.com>
2024-03-16 19:12:41 -0600

    change port to dev server

--- client-v2/src/api/mod.rs
+++ client-v2/src/api/mod.rs
@@ -6,23 +6,23 @@ use leptos::*;
 use crate::net::{request_signal::create_request, websocket_stream::create_websocket_stream};
 
 pub async fn create_contest() -> ContestFile {
-    let contest_message = create_request("http://localhost:9000/api/contest").await;
+    let contest_message = create_request("http://localhost:8000/api/contest").await;
 
     contest_message
 }
 
 pub async fn create_config() -> ConfigContest {
-    let config_message = create_request("http://localhost:9000/api/config").await;
+    let config_message = create_request("http://localhost:8000/api/config").await;
 
     config_message
 }
 
 pub fn create_runs() -> UnboundedReceiver<RunTuple> {
-    create_websocket_stream::<RunTuple>("ws://localhost:9000/api/allruns_ws")
+    create_websocket_stream::<RunTuple>("ws://localhost:8000/api/allruns_ws")
 }
 
 pub fn create_timer() -> ReadSignal<(TimerData, TimerData)> {
-    let mut timer_stream = create_websocket_stream::<TimerData>("ws://localhost:9000/api/timer");
+    let mut timer_stream = create_websocket_stream::<TimerData>("ws://localhost:8000/api/timer");
 
     let (timer, set_timer) = create_signal((TimerData::fake(), data::TimerData::new(0, 1)));
 
