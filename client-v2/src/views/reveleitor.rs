ab94ed85ca2ffc51d1e18db0afe46167d8655181
Emilio Wuerges <wuerges@gmail.com>
2024-03-23 14:21:16 -0300

    add reveleitor control panel

--- client-v2/src/views/reveleitor.rs
+++ client-v2/src/views/reveleitor.rs
@@ -30,6 +30,41 @@ pub fn RevelationPanel(driver: ReadSignal<RevelationDriver>, sede: Sede) -> impl
 }
 
 #[component]
+pub fn Control(driver: WriteSignal<RevelationDriver>) -> impl IntoView {
+    view! {
+        <div class="commandpanel">
+            <button on:click=move |_| { driver.update(|d| { d.reveal_step().ok(); })}>
+                {"←"}
+            </button>
+            <button on:click=move |_| { driver.update(|d| { d.reveal_step().ok(); })}>
+                {"→"}
+            </button>
+            <button on:click=move |_| { driver.update(|d| { d.restart(); d.reveal_top_n(100).ok(); })}>
+                Top 100
+            </button>
+            <button on:click=move |_| { driver.update(|d| { d.restart(); d.reveal_top_n(50).ok(); })}>
+                Top 50
+            </button>
+            <button on:click=move |_| { driver.update(|d| { d.restart(); d.reveal_top_n(30).ok(); })}>
+                Top 30
+            </button>
+            <button on:click=move |_| { driver.update(|d| { d.restart(); d.reveal_top_n(10).ok(); })}>
+                Top 10
+            </button>
+            <button on:click=move |_| { driver.update(|d| { d.reveal_top_n(0).ok(); })}>
+                All
+            </button>
+            <button on:click=move |_| { driver.update(|d| { d.restart(); })}>
+                Reset
+            </button>
+            <div>
+                Times: {0}
+            </div>
+        </div>
+    }
+}
+
+#[component]
 pub fn Revelation(
     sede: Sede,
     runs_file: RunsFile,
@@ -44,7 +79,10 @@ pub fn Revelation(
         driver.map(|driver| {
             let (get_driver, set_driver) = create_signal(driver);
 
-            view! { <RevelationPanel driver=get_driver sede=sede.clone() /> }
+            view! {
+                <Control driver=set_driver />
+                <RevelationPanel driver=get_driver sede=sede.clone() />
+            }
         })
     }
 }
