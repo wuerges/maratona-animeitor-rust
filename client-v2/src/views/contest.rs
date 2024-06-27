cc98894391bd2fcf3ef2e26261f15249b2fc9945
Emilio Wuerges <wuerges@gmail.com>
2024-06-27 20:45:52 -0300

    move problem to its own module

--- client-v2/src/views/contest.rs
+++ client-v2/src/views/contest.rs
@@ -11,6 +11,7 @@ use crate::{
     model::{runs_panel_signal::RunsPanelItemManager, ContestSignal, TeamSignal},
     views::{
         photos::{PhotoState, TeamPhoto},
+        problem::Problem,
         timer::Timer,
     },
 };
@@ -99,14 +100,6 @@ fn RunsPanel<'cs>(items: &'cs RunsPanelItemManager, sede: Signal<Rc<Sede>>) -> i
     }
 }
 
-fn number_submissions(s: usize) -> Option<usize> {
-    if s == 1 {
-        None
-    } else {
-        Some(s - 1)
-    }
-}
-
 fn nome_sede(sede: &Sede) -> &str {
     sede.entry.name.as_str()
 }
@@ -127,50 +120,6 @@ fn cell_top(i: usize, center: &Option<usize>) -> String {
                 format!("calc(var(--row-height) * {} + var(--root-top))", i + 9 - p)
             }
         }
-    }
-}
-
-#[component]
-fn Problem(prob: char, problem: Option<data::ProblemView>) -> impl IntoView {
-    // log!("rendered problem {:?}", problem);
-    view! {
-            <div class={match &problem {
-                Some(p) => if p.solved && p.solved_first {
-                    "star cell quadrado".to_string()
-                } else if p.solved {
-                    "accept cell quadrado".to_string()
-                } else {
-                    let cell_type = if p.wait { "inqueue"} else { "unsolved" };
-                    format!("cell quadrado {cell_type}")
-                },
-                None => "not-tried cell quadrado".to_string(),
-            }}>
-            {match &problem {
-                Some(p) => {
-                    (if p.solved {
-                        let balao = format!("balao_{}", prob);
-                        let img = if p.solved_first { "star-img"} else { "accept-img" };
-                        view! {
-                            <div class=format!("{img} {balao}")></div>
-                            <div class="accept-text cell-content">
-                                +{number_submissions(p.submissions)}<br />{p.time_solved}
-                            </div>
-                        }
-                    } else {
-                        let cell_symbol = if p.wait { "?" } else { "X" };
-
-                        view! {
-                            <div class="cima">{cell_symbol}</div>
-                            <div class="baixo">"("{p.submissions}")"</div>
-                        }
-                    }).into_view()
-
-                },
-                None => {
-                    {"-"}.into_view()
-                },
-            }}
-            </div>
     }
 }
 
--- client-v2/src/views/mod.rs
+++ client-v2/src/views/mod.rs
@@ -2,6 +2,7 @@ pub mod config;
 pub mod contest;
 pub mod navigation;
 pub mod photos;
+pub mod problem;
 pub mod reveleitor;
 pub mod sedes;
 pub mod timer;
