4abae648ed48d2d77ff2cf5a7b9ead4e028d6a32
Emilio Wuerges <wuerges@gmail.com>
2024-07-08 18:36:13 -0300

    add background color

--- client-v2/src/views/global_settings.rs
+++ client-v2/src/views/global_settings.rs
@@ -5,11 +5,15 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct GlobalSettings {
     pub mute: bool,
+    pub background_color: Option<String>,
 }
 
 impl Default for GlobalSettings {
     fn default() -> Self {
-        Self { mute: true }
+        Self {
+            mute: true,
+            background_color: None,
+        }
     }
 }
 
@@ -28,6 +32,14 @@ pub fn provide_global_settings() {
     })
 }
 
+fn maybe_color(text: String) -> Option<String> {
+    if text.is_empty() {
+        None
+    } else {
+        Some(text)
+    }
+}
+
 #[component]
 pub fn SettingsPanel() -> impl IntoView {
     let global = use_context::<GlobalSettingsSignal>().unwrap();
@@ -40,6 +52,14 @@ pub fn SettingsPanel() -> impl IntoView {
                     type="checkbox"
                     prop:checked=move || global.global.with(|g| g.mute)
                     on:input=move |ev| global.set_global.update(|g| g.mute = event_target_checked(&ev))
+                />
+            </div>
+            <div class="control">
+                <label>background_color</label>
+                <input
+                    type="text"
+                    prop:value=move || global.global.with(|g| g.background_color.clone().unwrap_or_default())
+                    on:input=move |ev| global.set_global.update(|g| g.background_color = maybe_color(event_target_value(&ev)))
                 />
             </div>
         </div>
