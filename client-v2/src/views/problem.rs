6105d74bb08a5236aa897c755cdd93e7c9c44219
Emilio Wuerges <wuerges@gmail.com>
2024-07-08 18:28:40 -0300

    pending string

--- client-v2/src/views/problem.rs
+++ client-v2/src/views/problem.rs
@@ -31,16 +31,26 @@ pub fn Problem(prob: char, problem: Option<data::ProblemView>) -> impl IntoView 
                         view! {
                             <div class=format!("{img} {balao}")></div>
                             <div class="accept-text cell-content">
-                                +{number_submissions(p.submissions)}<br />{p.time_solved}
+                                <div class="cima">
+                                    +{number_submissions(p.submissions)}
+                                </div>
+                                <div class="baixo">
+                                    {p.time_solved}
+                                </div>
                             </div>
                         }
                     } else {
-                        let cell_symbol = if p.wait { "?" } else { "X" };
-                        let pending = (p.pending > 0).then_some(format!("+{}", p.pending));
+                        let pending = match p.pending {
+                            0 => "X".to_string(),
+                            1 => "?".to_string(),
+                            n => format!("({})", n)
+                        };
+
+                        let cell_symbol = if p.wait { pending } else { "X".to_string() };
 
                         view! {
                             <div class="cima">{cell_symbol}</div>
-                            <div class="baixo">"("{p.submissions}{pending}")"</div>
+                            <div class="baixo"> +{p.submissions}" "</div>
                         }
                     }).into_view()
 
