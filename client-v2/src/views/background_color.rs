f5a2082e3f2803be3998d407c0dd2c0ebc78ad59
Emilio Wuerges <wuerges@gmail.com>
2024-08-17 16:25:12 -0300

    background-color from query

--- client-v2/src/views/background_color.rs
+++ client-v2/src/views/background_color.rs
@@ -1,5 +1,6 @@
 use leptos::*;
 use leptos_dom::logging::console_log;
+use leptos_router::use_query_map;
 use leptos_use::use_document;
 
 use crate::views::global_settings::use_global_settings;
@@ -8,8 +9,13 @@ use crate::views::global_settings::use_global_settings;
 pub fn BackgroundColor() -> impl IntoView {
     let settings = use_global_settings();
 
+    let query = use_query_map();
+    let query_bg = (move || query.with(|ps| ps.get("background-color").cloned())).into_signal();
+
     create_effect(move |_| {
-        let color = settings.global.with(|g| g.background_color.clone());
+        let color = query_bg
+            .get()
+            .or_else(|| settings.global.with(|g| g.background_color.clone()));
         let document = use_document();
 
         if let Some(body) = document.body() {
