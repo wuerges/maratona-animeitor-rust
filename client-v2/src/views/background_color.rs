f0079937764ac3679927e0a9aabe826bb6c7fe7d
Emilio Wuerges <wuerges@gmail.com>
2025-04-26 14:07:03 -0300

    update background color component

--- client-v2/src/views/background_color.rs
+++ client-v2/src/views/background_color.rs
@@ -1,6 +1,5 @@
-use leptos::*;
-use leptos_dom::logging::console_log;
-use leptos_router::use_query_map;
+use leptos::{logging::log, prelude::*};
+use leptos_router::hooks::use_query_map;
 use leptos_use::use_document;
 
 use crate::views::global_settings::use_global_settings;
@@ -10,9 +9,9 @@ pub fn BackgroundColor() -> impl IntoView {
     let settings = use_global_settings();
 
     let query = use_query_map();
-    let query_bg = (move || query.with(|ps| ps.get("background-color").cloned())).into_signal();
+    let query_bg = Signal::derive(move || query.with(|ps| ps.get("background-color")));
 
-    create_effect(move |_| {
+    Effect::new(move |_| {
         let color = query_bg
             .get()
             .or_else(|| settings.global.with(|g| g.background_color.clone()));
@@ -23,13 +22,13 @@ pub fn BackgroundColor() -> impl IntoView {
                 Some(color) => {
                     body.style()
                         .set_property("background-color", &color)
-                        .and_then(|()| Ok(console_log("updated background color")))
+                        .and_then(|()| Ok(log!("updated background color")))
                         .ok();
                 }
                 None => {
                     body.style()
                         .remove_property("background-color")
-                        .and_then(|x| Ok(console_log(&format!("removed background color: {}", x))))
+                        .and_then(|x| Ok(log!("removed background color: {}", x)))
                         .ok();
                 }
             }
