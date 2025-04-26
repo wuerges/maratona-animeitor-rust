2f376bc48340a4ebafedf21380bcfb3f89468406
Emilio Wuerges <wuerges@gmail.com>
2025-04-26 15:41:40 -0300

    missing fix config

--- client-v2/src/views/config.rs
+++ client-v2/src/views/config.rs
@@ -1,15 +1,16 @@
-use leptos::*;
+use leptos::prelude::*;
 
 use crate::api::{create_config, ContestQuery};
 
 #[component]
 pub fn Config(query: Signal<ContestQuery>) -> impl IntoView {
-    let config = create_resource(move || query.get(), |q| create_config(q));
+    let config = Resource::new_blocking(move || query.get(), |q| create_config(q));
 
     move || match config.get() {
         Some(config) => view! {
             <p> Config!: "`"{format!("{:#?}", config)}"'" </p>
-        },
-        None => view! {<p> Config is none =/ </p>},
+        }
+        .into_any(),
+        None => view! {<p> Config is none =/ </p>}.into_any(),
     }
 }
