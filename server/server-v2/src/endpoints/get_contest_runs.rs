f41d2ee4ca35f7cdb967be988f11e9c6b7531771
Emilio Wuerges <wuerges@gmail.com>
2025-09-06 19:05:09 -0300

    returning runs as websocket

---
+++ server/server-v2/src/endpoints/get_contest_runs.rs
@@ -0,0 +1,41 @@
+use actix_web::{HttpRequest, HttpResponse, get, web};
+use tokio::pin;
+use tokio_stream::StreamExt;
+use tracing::{debug, warn};
+
+use crate::app_data::AppData;
+
+#[get("/contests/{contest}/runs")]
+pub async fn get_contest_runs(
+    data: web::Data<AppData>,
+    contest: web::Path<String>,
+    body: web::Payload,
+    req: HttpRequest,
+) -> Result<HttpResponse, actix_web::Error> {
+    let contest = data.app_v2.get_contest(&contest).await?;
+
+    let (response, mut session, _msg_stream) = actix_ws::handle(&req, body)?;
+
+    actix_web::rt::spawn(async move {
+        let runs = contest.get_runs().await;
+        pin!(runs);
+
+        while let Some(batch) = runs.next().await {
+            let batch = batch.await;
+
+            for run in batch {
+                match serde_json::to_string(&run) {
+                    Err(err) => warn!(?err, ?run, "failed serializing run"),
+                    Ok(run) => {
+                        if let Err(err) = session.text(run).await {
+                            debug!(?err, "websocket closed");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    Ok(response)
+}
