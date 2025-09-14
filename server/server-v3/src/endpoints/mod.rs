047f3e7e25307ebada663391149978b110346587
Emilio Wuerges <wuerges@gmail.com>
2025-09-14 14:15:47 -0300

    listing contests

--- server/server-v3/src/endpoints/mod.rs
+++ server/server-v3/src/endpoints/mod.rs
@@ -3,10 +3,12 @@ use actix_web::web;
 pub mod contest_admin;
 mod get_contest_runs;
 mod get_contest_time;
+mod list_contests;
 
 pub fn as_service(service_config: &mut web::ServiceConfig) {
     service_config.service((
         get_contest_runs::get_contest_runs,
         get_contest_time::get_contest_time,
+        list_contests::list_contests,
     ));
 }
