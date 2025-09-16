df2178fa73b39eafdf278f59cde89932bfd6899e
Emilio Wuerges <wuerges@gmail.com>
2025-09-16 13:53:42 -0300

    changed to internal

--- server/server-v3/src/endpoints/api.rs
+++ server/server-v3/src/endpoints/api.rs
@@ -10,15 +10,33 @@ use utoipa::OpenApi;
     endpoints::get_contest_time::get_contest_time,
     endpoints::get_site_configuration::get_site_configuration,
     endpoints::list_contests::list_contests,
-    endpoints::contest_admin::update_contest_parameters,
-    endpoints::contest_admin::update_contest_sites,
-    endpoints::contest_admin::update_contest_state,
-    endpoints::contest_admin::create_contest,
+    open_api
 ))]
 struct ApiDoc;
 
 #[instrument(skip_all)]
+#[utoipa::path()]
 #[get("/openapi.json")]
 pub async fn open_api() -> Result<String, serde_json::Error> {
     ApiDoc::openapi().to_pretty_json()
+}
+
+#[derive(OpenApi)]
+#[openapi(paths(
+    endpoints::contest_admin::update_contest_parameters,
+    endpoints::contest_admin::update_contest_sites,
+    endpoints::contest_admin::update_contest_state,
+    endpoints::contest_admin::create_contest,
+    open_api_internal
+))]
+struct InternalApiDoc;
+
+#[instrument(skip_all)]
+#[utoipa::path(context_path = "/internal")]
+#[get("/openapi.json")]
+pub async fn open_api_internal() -> Result<String, serde_json::Error> {
+    let public = ApiDoc::openapi();
+    let internal = InternalApiDoc::openapi().merge_from(public);
+
+    internal.to_pretty_json()
 }
