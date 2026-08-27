a1c675609fc7228d8b11aa9e9891ed5c52ae2912
Emilio Wuerges <wuerges@gmail.com>
2026-08-27 12:47:38 -0300

    service: drop pair_arg, sentry and secret-file types moved to cli

--- server/service/src/lib.rs
+++ server/service/src/lib.rs
@@ -7,9 +7,7 @@ pub mod dbupdate_v2;
 pub mod errors;
 pub mod http;
 pub mod membroadcast;
-pub mod pair_arg;
 pub mod remote_control;
-pub mod sentry;
 pub mod volume;
 pub mod webcast;
 
--- server/service/src/sentry.rs
+++
@@ -1,7 +0,0 @@
-use sentry::ClientInitGuard;
-
-pub fn setup() -> ClientInitGuard {
-    sentry::init(
-        sentry::ClientOptions::default().release(sentry::release_name!().unwrap_or_default()),
-    )
-}
