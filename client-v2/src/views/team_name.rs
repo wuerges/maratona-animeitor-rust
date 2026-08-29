2b26c0d522c9a1d6098a1be9a7edab3ffb03897b
Emilio Wuerges <wuerges@gmail.com>
2026-08-29 14:59:23 -0300

    mostrando instituicao no placar

--- client-v2/src/views/team_name.rs
+++ client-v2/src/views/team_name.rs
@@ -3,10 +3,13 @@ use leptos::prelude::*;
 #[component]
 pub fn TeamName(escola: String, name: String) -> impl IntoView {
     let is_long = name.len() > 30;
+    let show_escola = !name.contains(&escola);
     view! {
         <div class="cell time">
-            <div class:nomeEscola=true >{escola}</div>
             <div class:nomeTime=true class:longTeamName=is_long >{name}</div>
+            {show_escola.then(|| view! {
+                <div class:nomeEscola=true >{escola}</div>
+            })}
         </div>
     }
 }
