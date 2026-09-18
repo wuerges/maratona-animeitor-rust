ef6bed250accb3241eba18e6396dd60051b68fd5
Emilio Wuerges <wuerges@gmail.com>
2026-09-18 15:03:29 -0300

    new timer

--- animeitor-client/src/views/countdown.rs
+++ animeitor-client/src/views/countdown.rs
@@ -6,11 +6,8 @@
 use data::TimerData;
 use leptos::prelude::*;
 
+use super::timer::Timer;
 use crate::api::EventContest;
-
-fn pad(n: u64) -> String {
-    format!("{n:0>2}")
-}
 
 #[component]
 pub fn Countdown(ec: EventContest, timer: ReadSignal<(TimerData, TimerData)>) -> impl IntoView {
@@ -20,21 +17,10 @@ pub fn Countdown(ec: EventContest, timer: ReadSignal<(TimerData, TimerData)>) ->
 
     view! {
         <div class="countdown">
+            <Timer timer />
             <div class="event-name">{event_name}</div>
             <div class="contest-name">{contest_name}</div>
-            <div class="remaining">
-                "Faltam "
-                {move || {
-                    let (time_data, _) = timer.get();
-                    let remaining = time_data.current_time.unsigned_abs();
-                    view! {
-                        <span class="horas">{pad(remaining / 3600)}</span>":"
-                        <span class="minutos">{pad((remaining / 60) % 60)}</span>":"
-                        <span class="segundos">{pad(remaining % 60)}</span>
-                    }
-                }}
-                " para o início"
-            </div>
+            <p class="countdown-message">"Aguardando o início da competição"</p>
         </div>
     }
 }
